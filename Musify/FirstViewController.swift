86c5f7a4d1ea4837f7688592a9e49d9d5438adb6
CS3714 <cs3714@torg1010-17.its.vt.edu>
2016-12-07 15:51:52 -0500

    Initial Commit

---
+++ Musify/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Musify
+//
+//  Created by CS3714 on 12/7/16.
+//  Copyright © 2016 Nicholas Hu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
